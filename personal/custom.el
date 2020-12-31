04bf4413db87c0f8fe1a6b713aeb200f6bc5b1db
Bill Sacks <sacks@ucar.edu>
2020-12-30 21:01:35 -0700

    Turn off size indication mode

--- personal/custom.el
+++ personal/custom.el
@@ -10,7 +10,7 @@
  '(package-selected-packages
    '(yaml-mode web-mode lsp-ui company-lsp json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window))
  '(prelude-guru nil)
- '(size-indication-mode t)
+ '(size-indication-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
