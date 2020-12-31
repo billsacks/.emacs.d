02151067daab03b94e1c98ab5a32d681802eef5a
Bill Sacks <sacks@ucar.edu>
2020-12-30 20:58:40 -0700

    Change how I'm disabling global-nlinum-mode
    It feels cleaner to do this via the customization interface than via a
    separate file

--- personal/custom.el
+++ personal/custom.el
@@ -6,6 +6,7 @@
  '(blink-cursor-mode t)
  '(column-number-mode t)
  '(cursor-type 'bar)
+ '(global-nlinum-mode nil)
  '(package-selected-packages
    '(yaml-mode web-mode lsp-ui company-lsp json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window))
  '(prelude-guru nil)
--- personal/disable.el
+++
@@ -1,1 +0,0 @@
-(global-nlinum-mode -1)
