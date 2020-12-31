33fa7e899cf87eb0f55a2939b6fc61fe2e577a7e
Bill Sacks <sacks@ucar.edu>
2020-12-31 06:45:16 -0700

    Add some key bindings for avy

--- personal/keybindings.el
+++ personal/keybindings.el
@@ -1,6 +1,11 @@
+;; Swiper key-bindings
 (global-set-key (kbd "C-s") 'swiper-isearch)
 ;; The point of the backward search is two-fold:
 ;; (1) because it's symmetrical with forward search
 ;; (2) it can be useful to go to the start of the search term rather
 ;;     than the end of the search term
 (global-set-key (kbd "C-r") 'swiper-isearch-backward)
+
+;; Avy key-bindings
+(global-set-key (kbd "M-g w") 'avy-goto-word-1)
+(global-set-key (kbd "M-g l") 'avy-goto-line)
