38f1bea9b1d9fdacb984b9a78c2ce02289c8ed81
Bill Sacks <sacks@ucar.edu>
2021-01-01 15:49:53 -0700

    Do not show . and .. in file-choosing buffer

--- personal/custom.el
+++ personal/custom.el
@@ -11,6 +11,7 @@
  '(flycheck-indication-mode 'left-margin)
  '(frame-resize-pixelwise t)
  '(global-nlinum-mode nil)
+ '(ivy-extra-directories nil)
  '(package-selected-packages
    '(projectile-ripgrep lsp-python-ms yaml-mode web-mode lsp-ui company-lsp json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window))
  '(prelude-clean-whitespace-on-save nil)
