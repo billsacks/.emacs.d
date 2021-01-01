0d8ae56ff11a2e678342e9eb0fc60802c56a0942
Bill Sacks <sacks@ucar.edu>
2021-01-01 12:28:41 -0700

    Allow resizing windows to full size of screen

--- personal/custom.el
+++ personal/custom.el
@@ -9,6 +9,7 @@
  '(column-number-mode t)
  '(cursor-type 'bar)
  '(flycheck-indication-mode 'left-margin)
+ '(frame-resize-pixelwise t)
  '(global-nlinum-mode nil)
  '(package-selected-packages
    '(projectile-ripgrep lsp-python-ms yaml-mode web-mode lsp-ui company-lsp json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window))
