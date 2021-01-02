161391eab75f05bd372c1a3545c148e7e26614a5
Bill Sacks <sacks@ucar.edu>
2021-01-02 00:38:17 -0700

    Define some useful macros

---
+++ personal/f90.el
@@ -0,0 +1,61 @@
+(require 'prelude-programming)
+
+;; Define a function to align all commas on a line
+;; adapted from http://www.emacswiki.org/emacs/AlignCommands
+(defun f90-align-args (start end)
+  "Align all commas on a line"
+  (interactive "r")
+  (align-regexp start end
+                "\\(\\s-*\\)," 1 0 t))
+
+;; Define a function to align the pieces of a Fortran argument list:
+;; align on comma, then '::', then '!'
+;; For some reason, this doesn't seem to work consistently (e.g., sometimes fails to work completely on the last line)
+(defun f90-align-arg-hdr (start end)
+  "Align the pieces of a Fortran argument list"
+  (interactive "r")
+  (align-regexp start end
+                "\\(\\s-*\\),")
+  (align-regexp start end
+                "\\(\\s-*\\)::")
+  (align-regexp start end
+                "\\(\\s-*\\)!"))
+
+;; Define a function to align the pieces of a Fortran variable declaration section:
+;; align on '::', then '!'
+(defun f90-align-var-decl (start end)
+  "Align the pieces of a fortran variable declaration section"
+  (interactive "r")
+  (align-regexp start end
+                "\\(\\s-*\\)::")
+  (align-regexp start end
+                "\\(\\s-*\\)!"))
+
+;; Define a function to align assertions
+(defun f90-align-asserts (start end)
+  "Align equal signs and errMsg portions of assertions"
+  (interactive "r")
+  (align-regexp start end
+                "\\(\\s-*\\)==")
+  (align-regexp start end
+                "\\(\\s-*\\)errMsg"))
+
+;; Define a function to align items in an associate clause
+;; (For best results, put a temporary comma before the '&' on the last line, which you can delete after running this function
+(defun f90-align-associate (start end)
+  "Align pointer and comma in associate clause"
+  (interactive "r")
+  (align-regexp start end
+                "\\(\\s-*\\)=>")
+  (align-regexp start end
+                "\\(\\s-*\\),"))
+
+(defun prelude-f90-mode-defaults ()
+  "Defaults for Fortran programming"
+  ;; currently doesn't do anything
+  )
+
+(setq prelude-f90-mode-hook 'prelude-f90-mode-defaults)
+
+(add-hook 'f90-mode-hook (lambda ()
+                           (run-hooks 'prelude-f90-mode-hook)))
---
+++ personal/macros.el
@@ -0,0 +1,22 @@
+;; general-purpose macros
+
+;; swap two strings (from http://stackoverflow.com/questions/768243/interactive-emacs-lisp-function-to-swap-two-words-with-each-other)
+(defun swap-words (a b)
+  "Replace all occurrences of a with b and vice versa"
+  (interactive "*sFirst Swap Word: \nsSecond Swap Word: ")
+  (save-excursion
+    (while (re-search-forward (concat (regexp-quote a) "\\|" (regexp-quote b)))
+      (if (y-or-n-p "Swap?")
+          (if (equal (match-string 0) a)
+              (replace-match (regexp-quote b))
+            (replace-match (regexp-quote a))))
+      )))
+
+;; Unfill region (from https://www.emacswiki.org/emacs/UnfillRegion)
+(defun unfill-region (beg end)
+  "Unfill the region, joining text paragraphs into a single
+    logical line.  This is useful, e.g., for use with
+    `visual-line-mode'."
+  (interactive "*r")
+  (let ((fill-column (point-max)))
+    (fill-region beg end)))
