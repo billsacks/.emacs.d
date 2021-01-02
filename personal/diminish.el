d7ee472d84b889f5cd3e423f86878f5d92e24005
Bill Sacks <sacks@ucar.edu>
2021-01-02 13:43:40 -0700

    Clean up clutter in mode line

---
+++ personal/diminish.el
@@ -0,0 +1,8 @@
+;; Clean up clutter in the mode line
+(diminish 'yas-minor-mode)
+(diminish 'company-mode)
+(diminish 'whitespace-mode)
+(diminish 'flyspell-mode)
+(diminish 'smartparens-mode)
+(diminish 'prelude-mode)
+(diminish 'which-key-mode)
