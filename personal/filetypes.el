a4f4ff61eea591a636c42bf205ae4fdddbbab34b
Bill Sacks <sacks@ucar.edu>
2021-01-02 02:10:52 -0700

    make .pf files open in f90 mode

--- personal/filetypes.el
+++ personal/filetypes.el
@@ -3,3 +3,6 @@
 ;; open ChangeLog files in text mode, because the CESM ChangeLog files
 ;; don't follow emacs' expected ChangeLog format
 (setq auto-mode-alist (cons '("ChangeLog" . text-mode) auto-mode-alist))
+
+;; make .pf files open in f90 mode
+(setq auto-mode-alist (cons '("\.pf$" . f90-mode) auto-mode-alist))
