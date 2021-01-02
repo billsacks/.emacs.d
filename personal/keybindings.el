8a3d1e45062a49e1c4e388f70ccbc2d15937dc1d
Bill Sacks <sacks@ucar.edu>
2021-01-02 10:05:23 -0700

    Modify / fix some key bindings

--- personal/keybindings.el
+++ personal/keybindings.el
@@ -10,11 +10,15 @@
 
 ;; Avy key-bindings
 (global-set-key (kbd "M-g w") 'avy-goto-word-1)
-(global-set-key (kbd "M-g l") 'avy-goto-line)
+;; Note that you can use this for goto-line by entering numbers, so it's safe to override
+;; the built-in M-g g for goto-line
+(global-set-key (kbd "M-g g") 'avy-goto-line)
 
 ;; Some other convenient shortcuts
 (global-set-key (kbd "<s-down>") (kbd "C-u 3 C-v"))
 (global-set-key (kbd "<s-up>") (kbd "C-u 3 M-v"))
 (global-set-key (kbd "C-c c") 'copy-current-line)
+(define-key prelude-mode-map (kbd "C-c f") nil)
 (global-set-key (kbd "C-c f") 'auto-fill-mode)
+(define-key prelude-mode-map (kbd "C-c t") nil)
 (global-set-key (kbd "C-c t") 'indent-relative)
