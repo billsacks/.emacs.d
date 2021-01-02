74e50939d21b568f5b803b58485a37e5cf84e7e0
Bill Sacks <sacks@ucar.edu>
2021-01-02 01:16:20 -0700

    Disable some aspect of auto-saving
    Following what was the case in Aquamacs

--- personal/custom.el
+++ personal/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-interval 0)
  '(avy-keys '(97 111 101 117 105 100 104 116 110 115))
  '(blink-cursor-blinks 2)
  '(blink-cursor-mode t)
