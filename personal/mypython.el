2d1e531bca34e7391fb7ab77c13b9c4d0e16a896
Bill Sacks <sacks@ucar.edu>
2021-01-08 20:43:48 -0700

    Change python customization
    Use my own python module in place of the out-of-the-box prelude python
    module. The main point is to use lsp rather than anaconda.
    
    This isn't yet tested.

---
+++ personal/mypython.el
@@ -0,0 +1,31 @@
+(require 'electric)
+(require 'prelude-programming)
+(require 'lsp-mode)
+(add-hook 'python-mode-hook #'lsp)
+
+;; from prelude-python.el
+(when (fboundp 'exec-path-from-shell-copy-env)
+  (exec-path-from-shell-copy-env "PYTHONPATH"))
+
+(defun my-python-mode-defaults ()
+  "Defaults for Python programming."
+  ;; should we include eldoc-mode? it's included in prelude-python.el, but that may be
+  ;; because it's connected to anaconda
+
+  ;; the following is all from prelude-python.el
+  (subword-mode +1)
+  (setq-local electric-layout-rules
+              '((?: . (lambda ()
+                        (and (zerop (first (syntax-ppss)))
+                             (python-info-statement-starts-block-p)
+                             'after)))))
+  (when (fboundp #'python-imenu-create-flat-index)
+    (setq-local imenu-create-index-function
+                #'python-imenu-create-flat-index))
+  (add-hook 'post-self-insert-hook
+            #'electric-layout-post-self-insert-function nil 'local))
+
+(setq my-python-mode-hook 'my-python-mode-defaults)
+
+(add-hook 'python-mode-hook (lambda ()
+                              (run-hooks 'my-python-mode-hook)))
