e0104c10ab9e6022f482ecc4ab9a2089bf1b9d79
Bill Sacks <sacks@ucar.edu>
2021-01-10 08:48:06 -0700

    Change mode line format to match what I had in Aquamacs
    I think this looks better

--- personal/custom.el
+++ personal/custom.el
@@ -84,6 +84,6 @@
  '(hl-line ((t (:inherit highlight :extend t :background "#e2eee2"))))
  '(hl-todo ((t (:inherit font-lock-comment-face :weight bold))))
  '(ivy-virtual ((t (:inherit font-lock-builtin-face :slant italic))))
- '(mode-line ((t (:inherit variable-pitch :background "grey75" :foreground "black" :box (:line-width -1 :style released-button) :height 0.9))))
+ '(mode-line ((t (:background "grey75" :foreground "black" :box (:line-width -1 :style released-button) :height 0.8 :family "Lucida Grande"))))
  '(mode-line-inactive ((t (:inherit mode-line :background "grey90" :foreground "grey20" :box (:line-width -1 :color "grey75")))))
  '(whitespace-trailing ((t (:background "tan1" :foreground "yellow" :weight bold)))))
