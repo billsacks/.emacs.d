5a0556df292cfe2d7d6ea2f81592da7ffe9306a5
Bill Sacks <sacks@ucar.edu>
2021-01-20 16:44:40 -0700

    Add some possible password prompts
    I was trying to get tramp working. This helps (at least it recognizes
    the password prompt), but I still get a hang when trying to connect to
    cheyenne.

--- personal/preload/early_settings.el
+++ personal/preload/early_settings.el
@@ -1,1 +1,5 @@
 (setq winner-dont-bind-my-keys t)
+
+;; this needs to be set before the (require 'tramp) done in prelude
+(add-to-list 'password-word-equivalents "Token_Response")
+(add-to-list 'password-word-equivalents "TokenResponse")
