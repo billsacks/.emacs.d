13cc821f0c54d1bb1ddc5b47b666c87589f2aaae
Bill Sacks <sacks@ucar.edu>
2021-01-19 17:02:00 -0700

    Add key binding to open grep result in same window

---
+++ personal/compilation-mode.el
@@ -0,0 +1,14 @@
+;; Use 'o' to open a compilation result, grep result, etc. in the same window as the
+;; compilation-mode (or grep-mode or whatever) buffer
+;;
+;; from https://emacs.stackexchange.com/questions/33857/open-search-result-in-the-same-window
+(defun my-compile-goto-error-same-window ()
+  (interactive)
+  (let ((display-buffer-overriding-action
+         '((display-buffer-reuse-window
+            display-buffer-same-window)
+           (inhibit-same-window . nil))))
+    (call-interactively #'compile-goto-error)))
+(defun my-compilation-mode-hook ()
+  (local-set-key (kbd "o") #'my-compile-goto-error-same-window))
+(add-hook 'compilation-mode-hook #'my-compilation-mode-hook)
