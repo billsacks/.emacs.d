adffd8870a37156ec2f30bc6739dd73339f54c8d
Bill Sacks <sacks@ucar.edu>
2021-01-23 20:31:26 -0700

    Remove some ns customizations
    I think these don't apply with the railwaycat mac emacs version. I was
    having some issues with A-m (and maybe some other A-* keys) producing
    special characters (like Greek letters) instead of doing their key
    binding. I'm hoping that removing these ns settings fixes the issue.

--- personal/custom.el
+++ personal/custom.el
@@ -73,8 +73,6 @@
    '("%e" mode-line-front-space mode-line-mule-info mode-line-client mode-line-modified mode-line-remote mode-line-frame-identification mode-line-buffer-identification "   " mode-line-position smartrep-mode-line-string "  " mode-line-modes mode-line-misc-info
      (vc-mode vc-mode)
      mode-line-end-spaces))
- '(ns-alternate-modifier 'alt)
- '(ns-command-modifier 'meta)
  '(nxml-attribute-indent 0)
  '(nxml-child-indent 2)
  '(nxml-outline-child-indent 3)
