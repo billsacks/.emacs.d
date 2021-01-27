6b4a85bd79e408eb455cde43221eb44a008be446
Bill Sacks <sacks@ucar.edu>
2021-01-26 20:19:10 -0700

    Disable lsp-ui-sideline
    I think I prefer seeing flycheck diagnostics in the minibuffer rather
    than in the "sideline". This disables other things in the sideline,
    too. I haven't played with this enough to know if I necessarily want to
    disable all of it, but my initial feeling is that the sideline stuff is
    too unpredictable in its location (since it tries to find a close line
    above the current line with enough whitespace... which may end up being
    far away or nowhere).

--- modules/prelude-lsp.el
+++ modules/prelude-lsp.el
@@ -48,7 +48,7 @@
 (define-key lsp-ui-mode-map (kbd "C-c C-l d") 'lsp-describe-thing-at-point)
 (define-key lsp-ui-mode-map (kbd "C-c C-l e") 'lsp-execute-code-action)
 
-(setq lsp-ui-sideline-enable t)
+(setq lsp-ui-sideline-enable nil)
 (setq lsp-ui-doc-enable t)
 (setq lsp-ui-peek-enable t)
 (setq lsp-ui-peek-always-show t)
