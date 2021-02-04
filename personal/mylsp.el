c4a5847192a6e15f4c593436022833785eb866eb
Bill Sacks <sacks@ucar.edu>
2021-02-04 09:08:05 -0700

    Revert "Add a comment"
    This reverts commit 185ee0b12a5e8f56b87648a5058cacea25626bc0.
    
    I realized that lsp-describe-thing-at-point DOES work for type-bound
    procedures in general, so there may be no need for my idea

--- personal/mylsp.el
+++ personal/mylsp.el
@@ -1,10 +1,5 @@
 (require 'lsp)
 
-;; I could imagine creating a similar function to this that does a go-to-definition in a
-;; little window created below. One big value of that would be to facilitate seeing the
-;; definition of a type-bound subroutine in Fortran, for which lsp-describe-thing-at-point
-;; currently doesn't work (although, to get this to work, I would need to chain
-;; go-to-definition and go-to-implementation, I think).
 (defun lsp-describe-thing-in-window-below ()
   (interactive)
   (split-window-below)
