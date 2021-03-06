b7e0ec9391e89281202ce19f41aaea0699115015
Bill Sacks <sacks@ucar.edu>
2021-03-05 21:11:28 -0700

    Use M-SPC and A-M-SPC for company, tab for yasnippet
    The primary motivation is to make it easier to invoke company
    completion; then I can return to the defaults of tab for yasnippet
    without conflict between yasnippet and company.
    
    Another motivation for this: I never felt like the bindings I had before
    were very intuitive; hopefully these will be better.

--- personal/company.el
+++ personal/company.el
@@ -1,29 +1,32 @@
 (require 'company)
 
-;; Allow starting company completion manually
-(global-set-key (kbd "C-<tab>") 'company-complete)
+;; Allow starting company completion manually. Now that I am back to using a delay, I want
+;; this to be really easy to press, because I envision often triggering completion
+;; manually. This key binding with M is also nice because M is used to select a particular
+;; completion, and my thumb will already be there.
+(global-set-key (kbd "M-SPC") 'company-complete)
 
 ;; Sometimes I just want to insert a newline, not have company pick the
 ;; current selection. Having return bound to company-complete-selection
 ;; is especially problematic when the top candidate is (currently typed
 ;; text) + (some suffix); this can particularly happen when candidates
 ;; are sorted by occurrence (or anything other than alphabetically, I
-;; guess). To get around this, use S-TAB rather than Ret for
-;; company-complete-selection. I also like this key binding because it
-;; is similar to TAB (which is used by default for
-;; company-complete-common).
-;;
-;; (Note, though, that in text terminals, the shifted things may not
-;; work (see
-;; http://ergoemacs.org/emacs_manual/elisp/Other-Char-Bits.html).)
+;; guess). To get around this, use A-M-SPC rather than Ret for
+;; company-complete-selection. I have chosen this binding to be similar to the binding I
+;; set above for company-complete.
 ;;
 ;; (See also https://emacs.stackexchange.com/questions/13286/how-can-i-stop-the-enter-key-from-triggering-a-completion-in-company-mode)
+;;
+;; Also note that I am leaving nothing bound to company-complete-common: this is bound to
+;; tab by default, but that is asymmetric with my new use of M-SPC bindings for
+;; completion; it doesn't feel necessary to have a special binding for
+;; company-complete-common since company-complete does that the first time it's run.
 (with-eval-after-load 'company
   (define-key company-active-map (kbd "<return>") nil)
   (define-key company-active-map (kbd "RET") nil)
-  (define-key company-active-map (kbd "S-TAB") 'company-complete-selection)
-  (define-key company-active-map [(shift tab)] 'company-complete-selection)
-  (define-key company-active-map [backtab] 'company-complete-selection)
+  (define-key company-active-map (kbd "<tab>") nil)
+  (define-key company-active-map (kbd "TAB") nil)
+  (define-key company-active-map (kbd "A-M-SPC") 'company-complete-selection)
 
   ;; Do not try to auto-complete comments or plain text
   ;; need to customize company-backends this way rather than via standard customization so
--- personal/yasnippet.el
+++ personal/yasnippet.el
@@ -2,24 +2,6 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 
-;; To avoid conflicts with company mode, use S-SPC in place of tab for
-;; yasnippet (and then use S-M-SPC where yasnippet used to use S-tab)
-;;
-;; (Note, though, that in text terminals, the shifted things may not
-;; work (see
-;; http://ergoemacs.org/emacs_manual/elisp/Other-Char-Bits.html).)
-(define-key yas-minor-mode-map (kbd "<tab>") nil)
-(define-key yas-minor-mode-map (kbd "TAB") nil)
-(define-key yas-minor-mode-map (kbd "S-SPC") yas-maybe-expand)
-(eval-after-load 'yasnippet
-  '(progn
-     (define-key yas-keymap [(tab)]       nil)
-     (define-key yas-keymap (kbd "TAB")   nil)
-     (define-key yas-keymap [(shift tab)] nil)
-     (define-key yas-keymap [backtab]     nil)
-     (define-key yas-keymap (kbd "S-SPC") 'yas-next-field-or-maybe-expand)
-     (define-key yas-keymap (kbd "S-M-SPC") 'yas-prev-field)))
-
 ;; If yasnippet is set to do any indentation (which typically is helpful), it
 ;; results in indented blank lines. Clean those up with this hook:
 (add-hook 'yas/after-exit-snippet-hook
