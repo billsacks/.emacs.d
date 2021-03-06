83ac0973ed52a9ec0569795071da14d198838633
Bill Sacks <sacks@ucar.edu>
2021-03-06 12:43:11 -0700

    Don't flip company tooltip

--- modules/prelude-company.el
+++ modules/prelude-company.el
@@ -39,10 +39,6 @@
 (setq company-tooltip-limit 10)
 (setq company-minimum-prefix-length 2)
 (setq company-tooltip-align-annotations t)
-;; invert the navigation direction if the the completion popup-isearch-match
-;; is displayed on top (happens near the bottom of windows)
-(setq company-tooltip-flip-when-above t)
-
 (global-company-mode 1)
 (diminish 'global-company-mode)
 
