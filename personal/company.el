cfe8006a11f4bd360413bdf0f2f42994d12e8db6
Bill Sacks <sacks@ucar.edu>
2021-03-08 12:45:33 -0700

    Add another way to abort company completion

--- personal/company.el
+++ personal/company.el
@@ -53,6 +53,8 @@
   (define-key company-active-map (kbd "TAB") nil)
   (define-key company-active-map (kbd "M-SPC") 'company-complete-selection)
   (define-key company-active-map (kbd "A-M-SPC") 'company-complete-common)
+  ;; C-g will quit, but it could be helpful to have an alternative way to cancel the completion, too
+  (define-key company-active-map (kbd "S-SPC") 'company-abort)
 
   ;; Do not try to auto-complete comments or plain text
   ;; need to customize company-backends this way rather than via standard customization so
