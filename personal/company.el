a2c9a068c156b0054efcbc228d1bf02a825d6615
Bill Sacks <sacks@ucar.edu>
2021-03-08 10:31:46 -0700

    Bind M-SPC to company-complete-selection
    With fuzzy matching, I'm not finding it too useful to complete a common
    prefix. So I more commonly want to complete with the current selection,
    so make that easier.
    
    I'm not positive I like this: it's nice that it's easier, but it feels
    less symmetrical with the operation of M-SPC before company has been
    started. So I may decide to change this back.
    
    But one rationale for this is: A-M-SPC now invokes company with dabbrev,
    and that's where I am more likely to want to use
    company-complete-common, so it makes sense for A-M-SPC to also do
    company-complete-common once it is invoked.
    
    Also add some comments.

--- personal/company.el
+++ personal/company.el
@@ -4,11 +4,20 @@
 ;; this to be really easy to press, because I envision often triggering completion
 ;; manually. This key binding with M is also nice because M is used to select a particular
 ;; completion, and my thumb will already be there.
+;;
+;; (2021-03-08) Now that I am no longer using a delay after all, I could consider making
+;; this harder to press, instead making my-company-complete-with-dabbrev easier.
 (global-set-key (kbd "M-SPC") 'company-complete)
 
 ;; When LSP is enabled, completion is done via the language server. This is often nice,
 ;; but sometimes I just want to be able to do dumb completion using available symbols. So
 ;; here's a function that will allow that when it would be helpful.
+;;
+;; I considered changing the backends list so that both capf and dabbrev-code are used at
+;; once, probably distinguished using the company-box extension. The problem with that,
+;; though, is that they operate differently in terms of fuzzy matching, so I felt like
+;; mixing them could be weird and hard to use. But I could see maybe doing that instead of
+;; (or in addition to?) having this separate function.
 (defun my-company-complete-with-dabbrev ()
   (interactive)
   ;; I also wanted to use a different company-tooltip face for this (e.g., background
@@ -32,26 +41,18 @@
 ;; is especially problematic when the top candidate is (currently typed
 ;; text) + (some suffix); this can particularly happen when candidates
 ;; are sorted by occurrence (or anything other than alphabetically, I
-;; guess). To get around this, use A-M-SPC rather than Ret for
+;; guess). To get around this, use M-SPC rather than Ret for
 ;; company-complete-selection. I have chosen this binding to be similar to the binding I
 ;; set above for company-complete.
 ;;
 ;; (See also https://emacs.stackexchange.com/questions/13286/how-can-i-stop-the-enter-key-from-triggering-a-completion-in-company-mode)
-;;
-;; Also note that I am leaving nothing bound to company-complete-common: this is bound to
-;; tab by default, but that is asymmetric with my new use of M-SPC bindings for
-;; completion; it doesn't feel necessary to have a special binding for
-;; company-complete-common since company-complete does that the first time it's run. (But
-;; I may decide that it's better to bind M-SPC to company-complete-common to avoid
-;; accidentally completing the current selection if I press it twice in a row; I'll have
-;; to see if it's a positive or negative to be able to do that keybinding twice to get the
-;; current selection.)
 (with-eval-after-load 'company
   (define-key company-active-map (kbd "<return>") nil)
   (define-key company-active-map (kbd "RET") nil)
   (define-key company-active-map (kbd "<tab>") nil)
   (define-key company-active-map (kbd "TAB") nil)
-  (define-key company-active-map (kbd "A-M-SPC") 'company-complete-selection)
+  (define-key company-active-map (kbd "M-SPC") 'company-complete-selection)
+  (define-key company-active-map (kbd "A-M-SPC") 'company-complete-common)
 
   ;; Do not try to auto-complete comments or plain text
   ;; need to customize company-backends this way rather than via standard customization so
