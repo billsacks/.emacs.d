9e690c801bd5d87e28d9624dfef264b9a93d1631
Bill Sacks <sacks@ucar.edu>
2021-05-13 06:59:09 -0600

    Introduce my own version of flycheck-mode-line-status-text
    For now this is identical to the original in flycheck.el. I'll change it next.

---
+++ personal/myflycheck.el
@@ -0,0 +1,21 @@
+(require 'flycheck)
+
+;; My version of flycheck-mode-line-status-text. See comments for what I'm changing.
+(defun my-flycheck-mode-line-status-text (&optional status)
+  "Get a text describing STATUS for use in the mode line.
+
+STATUS defaults to `flycheck-last-status-change' if omitted or
+nil."
+  (let ((text (pcase (or status flycheck-last-status-change)
+                (`not-checked "")
+                (`no-checker "-")
+                (`running "*")
+                (`errored "!")
+                (`finished
+                 (let-alist (flycheck-count-errors flycheck-current-errors)
+                   (if (or .error .warning)
+                       (format ":%s|%s" (or .error 0) (or .warning 0))
+                     "")))
+                (`interrupted ".")
+                (`suspicious "?"))))
+    (concat " " flycheck-mode-line-prefix text)))
