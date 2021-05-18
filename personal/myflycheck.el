1c103c3f876f4c8f5cde2955f093dfc4cbd217e5
Bill Sacks <sacks@ucar.edu>
2021-05-18 17:29:00 -0600

    Include lsp-flycheck-info-unnecessary in flycheck's info count
    This also includes general logic for including other lsp-specific
    flycheck error types in the appropriate count.

--- personal/myflycheck.el
+++ personal/myflycheck.el
@@ -1,6 +1,24 @@
 (require 'flycheck)
 
-;; My version of flycheck-mode-line-status-text. See comments for what I'm changing.
+(defun my-flycheck-dash-for-zero (val)
+  "Return value unless it's 0, in which case we return dash"
+  (if (eq 0 val)
+      '-
+    val)
+  )
+
+(defun my-flycheck-error-is-type-p (err err-type)
+  "Return t if the given err is of the given type (string), nil otherwise
+
+A key piece of this involves counting errors of type
+lsp-flycheck-ERRTYPE-* in addition to errors of type ERRTYPE.
+"
+  (let ((this-err-type (symbol-name (car err))))
+    (or (string-equal err-type this-err-type)
+        (string-prefix-p (format "lsp-flycheck-%s" err-type) this-err-type))
+    ))
+
+;; My version of flycheck-mode-line-status-text.
 (defun my-flycheck-mode-line-status-text (&optional status)
   "Get a text describing STATUS for use in the mode line.
 
@@ -12,8 +30,21 @@ nil."
                 (`running " +| +| +")
                 (`errored "!")
                 (`finished
-                 (let-alist (flycheck-count-errors flycheck-current-errors)
-                   (format "%2s|%2s|%2s" (or .error '-) (or .warning '-) (or .info '-))
+                 (let ((num_errors 0)
+                       (num_warnings 0)
+                       (num_infos 0))
+                   (dolist (err (flycheck-count-errors flycheck-current-errors))
+                     (cond ((my-flycheck-error-is-type-p err "error")
+                            (setq num_errors (+ num_errors (cdr err))))
+                           ((my-flycheck-error-is-type-p err "warning")
+                            (setq num_warnings (+ num_warnings (cdr err))))
+                           ((my-flycheck-error-is-type-p err "info")
+                            (setq num_infos (+ num_infos (cdr err))))))
+                   (format "%2s|%2s|%2s"
+                           (my-flycheck-dash-for-zero num_errors)
+                           (my-flycheck-dash-for-zero num_warnings)
+                           (my-flycheck-dash-for-zero num_infos)
+                           )
                    ))
                 (`interrupted ".")
                 (`suspicious "?"))))
