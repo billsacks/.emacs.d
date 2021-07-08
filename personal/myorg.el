4db9673afa06eda1484ffc3f88c2301559b93320
Bill Sacks <sacks@ucar.edu>
2021-07-08 11:36:44 -0600

    Org mode: add state for in progress and change state colors

--- personal/myorg.el
+++ personal/myorg.el
@@ -3,12 +3,12 @@
 (require 'org)
 (require 'deft)
 (setq org-todo-keywords '(
-                          (sequence "TODO(t)" "|" "DONE(d)" "CANCELED(c)")
+                          (sequence "TODO(t)" "PROG(p)" "|" "DONE(d)" "CANCELED(c)")
                           ))
 
 (setq org-todo-keyword-faces
-      '(("TODO" . "red") ("DONE" . "ForestGreen")
-        ("CANCELED" . "blue")))
+      '(("TODO" . "blue") ("PROG" . "red") ("DONE" . "ForestGreen")
+        ("CANCELED" . "tan1")))
 
 (global-set-key [remap org-set-tags-command] #'counsel-org-tag)
 (define-key deft-mode-map (kbd "<C-backspace>") 'deft-filter-decrement-word)
