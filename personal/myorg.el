b32b48b63cdb5ed20ebe905de12d14a5eee3b9d9
Bill Sacks <sacks@ucar.edu>
2021-07-20 09:26:44 -0600

    Org: swap C-c a and C-c A
    Rationale: the shifted one should be the more drastic one.

--- personal/myorg.el
+++ personal/myorg.el
@@ -13,8 +13,8 @@
 (global-set-key [remap org-set-tags-command] #'counsel-org-tag)
 (define-key deft-mode-map (kbd "<C-backspace>") 'deft-filter-decrement-word)
 
-(define-key org-mode-map (kbd "C-c a") 'org-archive-to-archive-sibling)
-(define-key org-mode-map (kbd "C-c A") 'org-toggle-archive-tag)
+(define-key org-mode-map (kbd "C-c a") 'org-toggle-archive-tag)
+(define-key org-mode-map (kbd "C-c A") 'org-archive-to-archive-sibling)
 
 ;; org-display-outline-text doesn't show the last level; fix that with this function
 (defun my-org-show-position-in-text () ;; display outline path of hierarchical headings
