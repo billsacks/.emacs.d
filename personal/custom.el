f26daa3e6748052e6dd88fd373cd0bddfa41c8f3
Bill Sacks <sacks@ucar.edu>
2021-07-21 21:56:25 -0600

    Org: for done statistics, count recursively
    This customization counts all subchildren, sub-subchildren, etc. in
    computing todo statistics. This is needed in order to capture things
    archived to an Archive sibling - but I think I like the behavior in
    general, even without that need.

--- personal/custom.el
+++ personal/custom.el
@@ -162,6 +162,7 @@
  '(org-export-with-sub-superscripts '{})
  '(org-fontify-quote-and-verse-blocks t)
  '(org-hide-emphasis-markers t)
+ '(org-hierarchical-todo-statistics nil)
  '(org-imenu-depth 3)
  '(org-inlinetask-default-state "TODO")
  '(org-list-indent-offset 2)
