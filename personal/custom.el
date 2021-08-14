66be28352e0b76c0c059ea3e12caaedd17d8c9c8
Bill Sacks <sacks@ucar.edu>
2021-08-13 21:50:42 -0600

    Make org-headline-done bold
    Point is to make the headlines for done tasks more easily readable. An
    alternative could be to use a slightly darker color.
    
    I still like keeping the org-done keywords themselves non-bold: I tried
    returning to them being bold but felt they stood out too much that way.

--- personal/custom.el
+++ personal/custom.el
@@ -304,6 +304,7 @@
  '(org-date ((t (:inherit font-lock-keyword-face :height 0.8))))
  '(org-done ((t (:foreground "ForestGreen"))))
  '(org-drawer ((t (:inherit shadow :height 0.8))))
+ '(org-headline-done ((t (:foreground "RosyBrown" :weight bold))))
  '(org-inlinetask ((t (:inherit font-lock-function-name-face))))
  '(org-level-1 ((t (:inherit outline-1 :extend nil :weight bold :family "CharterSpacedAbove"))))
  '(org-level-2 ((t (:inherit outline-2 :extend nil :weight bold :family "CharterSpacedAbove"))))
