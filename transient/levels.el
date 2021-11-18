12e88e8107766b60a9ad51d7e2ab6c1ef12317b5
Bill Sacks <sacks@ucar.edu>
2021-11-18 11:58:47 -0700

    Magit: Make --regexp-ignore-case accessible for git log

--- transient/levels.el
+++ transient/levels.el
@@ -1,4 +1,6 @@
 ((magit-log
+  (transient:magit-log:--regexp-ignore-case . 1)
   (transient:magit-log:--first-parent . 1))
  (magit-log-refresh
+  (transient:magit-log-refresh:--regexp-ignore-case . 1)
   (transient:magit-log-refresh:--ancestry-path . 1)))
