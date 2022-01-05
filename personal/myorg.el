37ff9f4dddaad1f0c4c7b0b046aa5731dc32d542
Bill Sacks <sacks@ucar.edu>
2022-01-05 12:42:16 -0700

    Add an org speed command to insert a new todo
    The original motivation was wanting to make it easier to add a task
    after the parent of the current task. I thought about making a dedicated
    command for that, but then thought it would be easiest to do that in two
    steps: go to the parent, then insert a todo. Now this can be done with
    C-c C-u (existing command to go to parent) then space. The ability to
    add a task with space seems very nice in general (similar to what I can
    do in Things).
    
    Also change the org speed command to display outline path from " " to
    "o" so I can use " " for my new command to insert a todo.

--- personal/myorg.el
+++ personal/myorg.el
@@ -505,4 +505,9 @@ Note: the force-heading piece of this is untested."
       '(("N" org-speed-move-safe 'my-org-next-visible-todo)
         ("P" org-speed-move-safe 'my-org-previous-visible-todo)
         ("F" org-speed-move-safe 'my-org-forward-todo-same-level)
-        ("B" org-speed-move-safe 'my-org-backward-todo-same-level)))
+        ("B" org-speed-move-safe 'my-org-backward-todo-same-level)
+        ;; by default, space does org-display-outline-path, but I want to use space to
+        ;; insert a new todo; "o" by default does org-open-at-point, but that isn't very
+        ;; useful, so I'll use that for org-display-outline-path
+        ("o" . org-display-outline-path)
+        (" " . my-org-insert-todo-heading-respect-content)))
