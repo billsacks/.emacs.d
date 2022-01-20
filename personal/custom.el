83fd6da3419f6eda89915159fddb213573f330e6
Bill Sacks <sacks@ucar.edu>
2022-01-20 07:08:12 -0700

    Org: set org-inlinetask-show-first-star to t
    The main motivation is to help delineate the body of inline tasks, since
    the body isn't indented (which may be a bug? but that's the behavior in
    out-of-the-box org with uncustomized emacs as well as with my
    customizations). The fact that it makes inline tasks stand out more
    seems like a possibly good thing as well.

--- personal/custom.el
+++ personal/custom.el
@@ -171,6 +171,7 @@
  '(org-image-actual-width '(400))
  '(org-imenu-depth 3)
  '(org-inlinetask-default-state "TODO")
+ '(org-inlinetask-show-first-star t)
  '(org-list-indent-offset 2)
  '(org-refile-targets '((nil :maxlevel . 8)))
  '(org-src-window-setup 'current-window)
