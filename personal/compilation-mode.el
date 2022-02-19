e648c231db8df8a6e5f273a43ebba9bb870728ff
Bill Sacks <sacks@ucar.edu>
2022-02-19 08:48:58 -0700

    For compile (including grep, etc.) add commands to go to next/prev error
    I often find myself wanting to go to the next error and switch to that
    buffer. These commands accomplish it.

--- personal/compilation-mode.el
+++ personal/compilation-mode.el
@@ -1,3 +1,5 @@
+(require 'compile)
+
 ;; Use 'o' to open a compilation result, grep result, etc. in the same window as the
 ;; compilation-mode (or grep-mode or whatever) buffer
 ;;
@@ -8,7 +10,19 @@
          '((display-buffer-same-window)
            (inhibit-same-window . nil))))
     (call-interactively #'compile-goto-error)))
+
+(defun my-compile-goto-next-error ()
+  (interactive)
+  (compilation-next-error 1)
+  (compile-goto-error))
+(defun my-compile-goto-previous-error ()
+  (interactive)
+  (compilation-previous-error 1)
+  (compile-goto-error))
+
 (defun my-compilation-mode-hook ()
   (local-set-key (kbd "<M-return>") #'my-compile-goto-error-same-window)
+  (local-set-key (kbd "N") #'my-compile-goto-next-error)
+  (local-set-key (kbd "P") #'my-compile-goto-previous-error)
   (hl-line-mode +1))
 (add-hook 'compilation-mode-hook #'my-compilation-mode-hook)
