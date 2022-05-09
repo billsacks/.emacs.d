53964d5da5d3bbd7887e5f820b1a26bb57c95c2e
Bill Sacks <sacks@ucar.edu>
2022-05-09 15:01:13 -0600

    Turn off auto-save messages

--- personal/custom.el
+++ personal/custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-save-interval 0)
+ '(auto-save-no-message t)
  '(avy-keys '(97 111 101 117 105 100 104 116 110 115))
  '(blink-cursor-blinks 2)
  '(blink-cursor-mode t)
