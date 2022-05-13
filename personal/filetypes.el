da91a81d37e536a705d97688fb7352d21a13876c
Bill Sacks <sacks@ucar.edu>
2022-05-12 21:58:31 -0600

    Open .C files in c++ mode

--- personal/filetypes.el
+++ personal/filetypes.el
@@ -4,5 +4,8 @@
 ;; don't follow emacs' expected ChangeLog format
 (setq auto-mode-alist (cons '("ChangeLog" . text-mode) auto-mode-alist))
 
+;; make .C files open in c++ mode
+(add-to-list 'auto-mode-alist '("\\.C\\'" . c++-mode))
+
 ;; make .pf files open in f90 mode
 (setq auto-mode-alist (cons '("\.pf$" . f90-mode) auto-mode-alist))
